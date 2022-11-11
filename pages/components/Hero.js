c31c44d8390e6718dca7a2cf0c59c0000111e2f5
Olamide Olatunde <apphle@Olamides-MacBook-Pro.local>
2022-11-11 09:58:49 +0200

    navbar

--- pages/components/Hero.js
+++ pages/components/Hero.js
@@ -7,18 +7,18 @@ import styled  from "styled-components"
 export default function Hero() {
   return (
     <div className="font-monolisa md:mx-20 mt-20">
-      <h1 className="text-[#EEA47F] text-xs md:mx-14 mx-6">OLATUNDE OLAMIDE VICTOR</h1>
-      <h1 className="md:text-4xl text-3xl text-[#EAEAEA] mt-4 md:mx-14 mx-6">
+      <h1 className="text-[#EEA47F] text-xs md:mx-14 mx-2">OLATUNDE OLAMIDE VICTOR</h1>
+      <h1 className="md:text-4xl text-3xl text-[#EAEAEA] mt-4 md:mx-14 mx-2">
         Software Engineer creating impactful Software solutions with an amazing
         user experience
       </h1>
-      <p className="text-sm text-[#AEAEAE] md:mx-14 mx-6 md:mt-6 mt-3">
+      <p className="text-sm text-[#AEAEAE] md:mx-14 mx-2 md:mt-6 mt-3">
         I’m Victor Olamide Olatunde, a Software Engineer based in Cyprus, with
         over 4 years of Experience.I specialise in the front-end development of
         websites, mobile apps, and desktop applications, keeping simplicity and
         speed as my top targets in development :){" "}
       </p>
-      <Wrapper className="p-3 px-4 rounded-full border border-[#4D4D4D] bg-[#191919] hover:bg-[#1e1e1e] cursor-pointer text-sm md:mx-14 mx-6 mt-8 text-[#ffffff]">Email me</Wrapper>
+      <Wrapper className="p-3 px-4 rounded-full border border-[#4D4D4D] bg-[#191919] hover:bg-[#1e1e1e] cursor-pointer text-sm md:mx-14 mx-2 mt-8 text-[#ffffff]">Email me</Wrapper>
     </div>
   );
 }
--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Image from "next/image";
 import Icon from "../../public/images/icon.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,14 +8,16 @@ import {
   faSearch,
   faAmbulance,
   faAnchor,
-  faBars
+  faBars,
+  faXmark
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className='bg-[#111111]'>
+    <div className='bg-[#111111] relative'>
            {/*Header Section i.e Navbar */}
-      <div className="flex justify-between items-center p-3 mx-4">
+      <div className="flex justify-between items-center p-3 mx-4 opacity-95">
         <Image src={Icon} alt="Icon" height={30} width={30}></Image>
         <div className="flex items-center">
           <ul className="md:flex justify-between items-center space-x-10 font-monolisa text-xs font-light text-[#EAEAEA] mx-2 hidden">
@@ -32,15 +34,43 @@ export default function Navbar() {
               <Link href="/construction">Contact</Link>
             </li>
           </ul>
-          <div className='flex md:hidden'>
+          <div className='flex md:hidden cursor-pointer'>
           <FontAwesomeIcon
             icon={faBars}
             style={{ fontSize: 14, color: "white" }}
+            onClick={() => setOpen(!open)}
           />
           </div>
         </div>
       </div>
         {/*Header Section i.e Navbar */}
+
+        {/*Mobile Navbar*/}
+        <div className={`${
+          open? "md:hidden block bg-black translate-x-0 float-right fixed right-0 top-0 z-10 h-full w-fit animate-in ease-in-out duration-700 fade-out-95 zoom-in-95 slide-in-from-right-72"
+            : "hidden translate-x-full"
+        } ease-in-out duration-700` }>
+        <ul className="items-start space-y-10 p-20 font-monolisa text-xs font-light text-[#EAEAEA] mx-2">
+            <div className='cursor-pointer'> 
+              <FontAwesomeIcon
+            icon={faXmark}
+            style={{ fontSize: 14, color: "white" }}
+            onClick={() => setOpen(!open)}
+          /></div>
+            <li className='hover:text-[#EEA47F] font-monolisa cursor-pointer'>
+            <Link href="/">Home</Link>
+            </li>
+            <li className='cursor-pointer hover:text-[#EEA47F]'>
+              <Link href="/about">Professional Summary</Link>
+            </li>
+            <li className='cursor-pointer hover:text-[#EEA47F]'>
+              <Link href="/construction">Projects</Link>
+            </li>
+            <li className='cursor-pointer hover:text-[#EEA47F]'>
+              <Link href="/construction">Contact</Link>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }
